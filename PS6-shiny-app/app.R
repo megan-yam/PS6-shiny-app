5b6261c798ad75b19ece5959d089701cf9132d6f
megan-yam <meganyammm@gmail.com>
2023-03-04 15:48:46 -0800

    finished plot section?

--- PS6-shiny-app/app.R
+++ PS6-shiny-app/app.R
@@ -22,18 +22,70 @@ ui <- fluidPage(
                 October 26th, 2017, and infomation regarding their gameplay."),
              p("Here is a small random sample of data!"),
              tableOutput("sample")),
-    tabPanel("Plots"),
-    tabPanel("Tables")
+    tabPanel("Plot", 
+             sidebarLayout(
+               sidebarPanel(
+                 radioButtons("color",
+                              "What color pallet would you like to see the data
+                               presented in: ",
+                              choices = c("pink", "black"),
+                              selected = "pink"),
+                 selectInput("independent",
+                                    "What would you like the x-axis to be: ",
+                                    choices = c("country_rank",
+                                                "accuracy", "play_count", "level",
+                                                "hours", "performance_points",
+                                                "ranked_score", "ss", "ss", "s",
+                                                "a", "watched_by", "total_hits"),
+                                    selected = "country_rank")
+               ),
+               mainPanel(
+                 plotOutput("distPlot"),
+                 textOutput("plotObservation")
+               )
+             )
+    ),
+    tabPanel("Tables",
+             sidebarLayout(
+               sidebarPanel(
+                 radioButtons("tableValues",
+                              "What data would you like to see?",
+                              choices = c("country", "device"),
+                              selected = "country")
+               ),
+               mainPanel(
+                 
+               )
+             )
+      )
   )
 )
 
 server <- function(input, output) {
 
-    output$sample <- renderTable(
+    output$sample <- renderTable({
         osuranking %>%
           sample_n(size = 5, replace = TRUE) %>%
           arrange(rank)
-    )
+    })
+    
+    output$distPlot <- renderPlot({
+      osuranking %>% 
+        ggplot(aes(x = rank, y = get(input$independent), color = input$color)) +
+        geom_point() +
+        labs(title = paste("osu! rank according to", input$independent), 
+             x = paste(input$independent),
+             y = "osu! rank",
+             color = "color")
+    })
+    
+    output$plotObservation <- renderPrint({
+      num <- osuranking %>% 
+        select(input$independent) %>%
+        nrow()
+      cat("There are ", num, " observations contributing to this plot")
+    })
+    
     
 }
 
